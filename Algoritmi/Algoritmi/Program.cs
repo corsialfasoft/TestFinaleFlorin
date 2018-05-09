bf894238f5e31e3b0d4fc733517a81d4053165ba
brolin2 <gheliuc.florin@gmail.com>
2018-05-09 14:37:40 +0200

    Esercizio 2 , metodo Es2

--- Algoritmi/Algoritmi/Program.cs
+++ Algoritmi/Algoritmi/Program.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Algoritmi {
 	class Program {
 		static void Main(string[] args) {
-			Es1();
+			//Es1();
+			int res = Es2("Nel del cammin  della mia vita mi  ritrovai a far codice su .NET" ,"mezzo");
+			Console.WriteLine("Occorrenze = {0}",res);
 		}
 		public static void Es1(){
 			int count = 0;
@@ -26,6 +27,14 @@ namespace Algoritmi {
 					count--;
 				}
 			}
+		}
+		public static int Es2(string testo , string cerca){
+			string [] test = testo.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',',' ' });
+			var result = from parola in test where parola.ToLowerInvariant()==cerca.ToLowerInvariant()
+							select parola;
+			int risultato = result.Count();
+			Console.WriteLine("La Parola {0} è presente {1} volte",cerca,risultato);
+			return risultato;
 		}
 	}
 }
